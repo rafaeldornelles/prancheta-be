feb4ebcf0d0637607d63efd59c6d407fc318145c
Rafael Ferreira Dornelles <rafaeld@trt4.gov.br>
2023-09-19 16:37:15 -0300

    inserir documentação swagger

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:18-alpine
 
 COPY /dist ./dist
 COPY package.json ./dist
+COPY swagger-ui.yaml .
 RUN cd dist && npm i && ls
 
 CMD ["node", "dist/index.js"]
--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,17 @@ import { projectRouter } from "./routes/project.routes"
 import { briefingRouter } from "./routes/briefing.routes"
 
 export const app = express()
+const swaggerUI = require("swagger-ui-express")
+const YAML = require("yamljs")
+const swaggerConfig = YAML.load("swagger-ui.yaml")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig))
 app.use("/user", userRouter)
 
-app.use(verifyToken)
-app.use("/project", projectRouter)
-app.use("/briefing", briefingRouter)
+app.use("/project", verifyToken, projectRouter)
+app.use("/briefing", verifyToken, briefingRouter)
 
 app.use(handleError)
