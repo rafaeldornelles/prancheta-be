ff94b9e44cc9a1630727d0a2106b2712f2dd2ec5
Rafael <rafafd@hotmail.com>
2023-11-16 01:41:41 -0300

    adicionar cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from "express"
+
 import { userRouter } from "./routes/user.routes"
 import { handleError } from "./middleware/error.handler"
 import { verifyToken } from "./middleware/verify.middleware"
@@ -7,10 +8,12 @@ import { briefingRouter } from "./routes/briefing.routes"
 import { projectStepRouter } from "./routes/projectstep.routes"
 
 export const app = express()
+const cors = require("cors")
 const swaggerUI = require("swagger-ui-express")
 const YAML = require("yamljs")
 const swaggerConfig = YAML.load("swagger-ui.yaml")
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
 
